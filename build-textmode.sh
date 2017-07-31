c6829a57e0d5bb9d666698b591c0f081c02b406a
nullifiedcat <nullifiedcat@gmail.com>
2017-07-31 13:13:57 +0300

    locate + build

---
+++ build-textmode.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+#	This script creates a new group and adds current user to it
+#
+
+numcpu=$(grep -c ^processor /proc/cpuinfo)
+cathook=$(cat cathook-location)
+
+if [ $EUID == 0 ]; then
+	echo "This script must not be run as root"
+	exit
+fi
+
+if ! [ -e "$cathook/" ] || ! [ -e "$cathook/.git" ] || ! [ -e "$cathook/source-sdk-2013" ]; then
+	echo "Specified path doesn't point to cathook directory"
+	exit
+fi
+
+echo "WARNING: Your current build of cathook will be deleted"
+echo "You will have to re-compile cathook after this"
+read -p "Press enter to continue or Ctrl+C (close) to stop."
+
+mkdir -p "cathook"
+rm "cathook/libcathook.so"
+
+pushd "$cathook"
+
+make clean
+make -j$numcpu -e GAME=tf2 TEXTMODE=1 NO_WARNINGS=1 BUILD_DEBUG=1
+
+if ! [ -e "bin/libcathook.so" ]; then
+	echo "FATAL: Build failed"
+	exit
+fi
+
+popd
+
+cp "$cathook/bin/libcathook.so" cathook
---
+++ locate-cathook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+#	This script creates a new group and adds current user to it
+#
+
+if [ $EUID == 0 ]; then
+	echo "This script must not be run as root"
+	exit
+fi
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage:"
+	echo "    $0 \"/path/to/cathook\""
+	echo "Example:"
+	echo "    $0 \"/home/nullifiedcat/cathook\""
+	exit
+fi
+
+if ! [ -e "$1/" ] || ! [ -e "$1/.git" ] || ! [ -e "$1/source-sdk-2013" ]; then
+	echo "Specified path doesn't point to cathook directory"
+	exit
+fi
+
+echo "Is this the correct path?"
+echo "$1"
+read -p "Press enter to continue or Ctrl+C (close) to stop."
+echo "$1" > cathook-location
