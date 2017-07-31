7310d23b6fcaa24817f1d23eb8a6afdde7e68522
nullifiedcat <nullifiedcat@gmail.com>
2017-07-31 13:27:51 +0300

    start-steams

--- create-12-users.sh
+++ create-12-users.sh
@@ -13,9 +13,8 @@ read -p "Press ENTER to continue"
 for i in {0..12}
 do
 	echo "Creating user catbot-$i"
-	echo "ln -s \"$steamapps\" \"/home/catbot-$i/.local/share/Steam/steamapps\""
-#	sudo useradd -m catbot-$i
-#	sudo usermod -g catbots catbot-$i
-#	sudo -H -u steam-$1 bash -c "mkdir -p /home/catbot-$i/.local/share/Steam"
-#	sudo -H -u steam-$1 bash -c "ln -s '/mnt/encrypted-data/SteamLibrary/steamapps' /home/steam-$1/.local/share/Steam/steamapps"
+	sudo useradd -m catbot-$i
+	sudo usermod -g catbots catbot-$i
+	sudo -H -u steam-$1 bash -c "mkdir -p /home/catbot-$i/.local/share/Steam"
+	sudo -H -u steam-$1 bash -c "ln -s \"$steamapps\" \"/home/catbot-$1/.local/share/Steam/steamapps\""
 done
---
+++ start-steams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+#	This script starts N steam instances, 6 by default
+#
+
+max=6
+
+if [ "$#" == 1 ]; then
+	max=$1
+fi
+
+# Required for steam to start as other users
+xhost + >/dev/null
+
+for i in $(seq 1 $max)
+do
+	echo "Starting Steam for catbot $i"
+	echo "sudo -H -u otheruser bash -c 'steam &>/dev/null &'" 
+done
